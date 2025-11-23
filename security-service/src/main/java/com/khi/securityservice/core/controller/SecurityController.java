667499e8a8df3513640836dfc515d414d299e994
leesy010504 <leesy010504@gmail.com>
2025-11-24 01:29:47 +0900

    feat: user정보 받는 로직 추가

--- security-service/src/main/java/com/khi/securityservice/core/controller/SecurityController.java
+++ security-service/src/main/java/com/khi/securityservice/core/controller/SecurityController.java
@@ -1,16 +1,28 @@
 package com.khi.securityservice.core.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.khi.securityservice.core.controller.dto.UserInfo;
+import com.khi.securityservice.core.util.UserUtil;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/security")
 public class SecurityController {
+    //다른 모듈에서 유저 정보를 가져오기 위한 컨트롤러로, 외부에 노출되지 않습니다.
 
-    @GetMapping("/test")
-    public String test() {
-
-        return "ok";
+    private final UserUtil userUtil;
+    public SecurityController(UserUtil userUtil) {
+        this.userUtil = userUtil;
     }
+
+    @GetMapping("/users/{userId}")
+    public UserInfo getUserInfo(@PathVariable("userId") String userId){
+        return userUtil.getUserInfo(userId);
+    };
+
+    @GetMapping("/users/batch")
+    public List<UserInfo> getUserInfos(@RequestParam("userIds") List<String> userIds){
+        return userUtil.getUserInfos(userIds);
+    };
 }
---
+++ security-service/src/main/java/com/khi/securityservice/core/util/UserUtil.java
@@ -0,0 +1,34 @@
+package com.khi.securityservice.core.util;
+
+import com.khi.securityservice.core.controller.dto.UserInfo;
+import com.khi.securityservice.core.entity.domain.UserEntity;
+import com.khi.securityservice.core.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserUtil {
+
+    private final UserRepository userRepository;
+
+    public UserUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserInfo getUserInfo(String userId) {
+        UserEntity userEntity = userRepository.findByUid(userId);
+        return UserInfo.builder()
+                .role(userEntity.getRole())
+                .nickname("userEntity.getNickname()")
+                .userId(userId)
+                .name("userEntity.getName()")
+                .build();
+    }
+
+    public List<UserInfo> getUserInfos(List<String> userIds) {
+        return userIds.stream()
+                .map(this::getUserInfo)
+                .collect(Collectors.toList());
+    }
+}
