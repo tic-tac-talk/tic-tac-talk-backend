fd35a18c9ebe9cb9d90d4b1952ef4cf755f57aae
leesy010504 <148897132+leesy010504@users.noreply.github.com>
2025-11-27 00:17:42 +0900

    Update ChatServiceApplication.java

--- chat-service/src/main/java/com/khi/chatservice/ChatServiceApplication.java
+++ chat-service/src/main/java/com/khi/chatservice/ChatServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
-// cicd test
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableAsync
