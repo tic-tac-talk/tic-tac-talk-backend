d70aa61f3117e9c7a21b1e884116886ef4914fae
kokeunho <kokangsik65@naver.com>
2026-01-01 01:10:08 +0900

    fix: StompHeaderAccessor MessagesHeaderAccessor에서 가져오도록 수정

--- rag-service/src/main/java/com/khi/ragservice/service/event/WsSessionListener.java
+++ rag-service/src/main/java/com/khi/ragservice/service/event/WsSessionListener.java
@@ -3,9 +3,11 @@ package com.khi.ragservice.service.event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
 import java.security.Principal;
 
@@ -15,12 +17,28 @@ public class WsSessionListener {
 
     // ws 연결 시 로그 출력
     @EventListener
-    public void onConnect(SessionConnectEvent event) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
-        Principal principal = accessor.getUser();
+    public void onConnect(SessionConnectedEvent event) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(event.getMessage(), StompHeaderAccessor.class);
 
-        log.info("[RAG WS CONNECT] userName = {} 과 ws 연결됨",
-                principal != null ? principal.getName() : "null");
+        if (accessor == null) return;
+
+        Object userId = accessor.getSessionAttributes().get("userId");
+
+        log.info("[RAG WS] userName = {} 과 ws 연결됨", userId);
+    }
+
+    @EventListener
+    public void onSubscribe(SessionSubscribeEvent event) {
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(event.getMessage(), StompHeaderAccessor.class);
+
+        if (accessor == null) return;
+
+        Object userId = accessor.getSessionAttributes().get("userId");
+        String destination = accessor.getDestination();
+
+        log.info("[RAG WS]");
+        log.info(" - userId : {}", userId);
+        log.info(" - destination : {}", destination);
     }
 
     // ws 연결 해제 시 로그 출력
@@ -28,6 +46,6 @@ public class WsSessionListener {
     public void onDisconnect(SessionDisconnectEvent event) {
         String sessionId = event.getSessionId();
 
-        log.info("[RAG WS DISCONNECT] principal = {} 과 ws 연결 해제됨.", sessionId);
+        log.info("[RAG WS] principal = {} 과 ws 연결 해제됨.", sessionId);
     }
 }
